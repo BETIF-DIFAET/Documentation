1d08d7ee5d700ce2ce96b198d882965f9943cf38
Tommaso Diotalevi <tommaso.diotalevi@gmail.com>
2025-07-25 19:11:53 +0200

    Add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ templates_path = ['_templates']
 
 #html_theme = 'sphinx_rtd_theme'
 html_theme = 'furo'
+html_logo = "logo-ET-bologna.png"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
